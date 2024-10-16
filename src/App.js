112b1fcfab053a38cb1a9d97bbc9676b89648a8a
DevDuck-JS <103818112@student.swin.edu.au>
2024-10-16 11:03:39 +1100

    New branch

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code>Convert the Temperature! Let's try again!</code>
+          <code>Convert the Temperature! New branch</code>
         </p>
 
         <br />
